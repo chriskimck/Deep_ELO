5942cdb012f69640c867dbcf6ff0445ce68a805d
ceegeechow <camille@chowww.com>
2018-11-29 15:16:56 -0500

    added dropout to reduce overfit, mse: white: 165901 , black: 167036

--- pad1000_model.py
+++ pad1000_model.py
@@ -53,7 +53,9 @@ x_train, x_val, y_train, y_val = train_test_split(x_train,y_train, test_size = 0
 # model
 model = Sequential()
 model.add(Dense(512, input_dim=PAD_LEN, activation='relu'))
+model.add(Dropout(.25))
 model.add(Dense(1024, activation='relu'))
+model.add(Dropout(.35))
 model.add(Dense(2, activation='relu'))
 model.summary()
 
