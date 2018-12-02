51157c3ccde8db1348372123c69364fca43d7b60
ceegeechow <camille@chowww.com>
2018-12-02 14:33:48 -0500

    winning ELO first, padding with last score instead of 1000, using keras optimizers, MAE: 198 and 214

--- model.py
+++ model.py
@@ -8,17 +8,18 @@ from keras.layers import Embedding, Flatten, Dense, Dropout
 
 NUM_GAMES = 24978 # Events 625,1541,2962,3904,5415,6698,7386,8655 were taken out (empty games)
 PAD_LEN = 329
-PAD_SCORE = 850
+# PAD_SCORE = 850
 
 data = pd.read_csv("data.csv") # contains white/black elo and stockfish scores for each game
 move_scores_df = data["MoveScores"]
 white_elo = data["WhiteElo"]
 black_elo = data["BlackElo"]
 
-move_scores = [] # training stockfish data
+move_scores = [] #training stockfish data
 temp = []
+elos = []
+i = 0
 
-# get move scores from each game and pad to PAD_LEN
 for line in move_scores_df:
 	for s in line.split(' '):
 		if s == 'NA':
@@ -30,20 +31,28 @@ for line in move_scores_df:
 	
 	if int(temp[-1]) > 0:
 		temp += [PAD_SCORE]*(PAD_LEN-len(temp))
+		elos.append([int(white_elo[i]),int(black_elo[i])])
+	
 	elif int(temp[-1]) < 0:
 		temp += [-PAD_SCORE]*(PAD_LEN-len(temp))
-	else:
-		temp += [0]*(PAD_LEN-len(temp))
+		elos.append([int(black_elo[i]),int(white_elo[i])])
 	
+	else:
+		j = np.random.randint(2)
+		temp += [0]*(PAD_LEN-len(temp))
+		if j == 1:
+			elos.append([int(white_elo[i]),int(black_elo[i])])
+		else:
+			elos.append([int(black_elo[i]),int(white_elo[i])])
+	
+	i+=1
 	temp = list(map(int,temp))
 	move_scores.append(temp)
 	temp = []
 
-# get labels as tuple: (white elo, black elo)
-elos = []
-for i in range(NUM_GAMES):
-	elos.append([int(white_elo[i]),int(black_elo[i])])
 
+# for i in range(NUM_GAMES):
+# 	elos.append([int(white_elo[i]),int(black_elo[i])])
 X = np.asarray(move_scores)
 Y = np.asarray(elos)
 
@@ -53,15 +62,16 @@ x_train, x_val, y_train, y_val = train_test_split(x_train,y_train, test_size = 0
 
 # model
 model = Sequential()
+# keras.layers.normalization.BatchNormalization(epsilon=1e-06, mode=0, momentum=0.9, weights=None)
 model.add(Dense(512, input_dim=PAD_LEN, activation='relu'))
 model.add(Dropout(.25))
 model.add(Dense(1024, activation='elu'))
 model.add(Dropout(.35))
 model.add(Dense(2, activation='relu'))
-model.summary()
+# model.summary()
 
-# train
-model.compile(optimizer=tf.train.AdamOptimizer(), loss='mean_squared_error', metrics=['accuracy'])
+optim = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(optimizer=optim, loss='mean_squared_error', metrics=['accuracy'])
 history = model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_val, y_val), verbose=1)
 prediction = model.predict(x_test)
 
