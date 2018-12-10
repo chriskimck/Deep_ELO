cc9833488c5e5cf4a9d05cb98c823b5e7123627a
Christopher Kim <christopherkim511@gmail.com>
2018-12-10 14:58:45 -0500

    Cleaning up codebase

--- chess_dataprocessing.py
+++ chess_dataprocessing.py
@@ -12,13 +12,14 @@ WhiteElo = []
 BlackElo = []
 moves = []
 movescores = []
+Event_Test = []
+moves_Test = []
+movescores_Test = []
 emptygames = [625,1541,2962,3904,5415,6698,7386,8655,8916,10505,10549,12872,13338,14483,17029,17051,18215,18829,20919,21585,21673,21676]
 
-for i in range(25000):
+for i in range(50000):
 	game = chess.pgn.read_game(pgn)
-	if (i+1) in emptygames:
-		continue
-	else:
+	if i < 25000:
 		Event.append(int(game.headers["Event"]))
 		WhiteElo.append(int(game.headers["WhiteElo"]))
 		BlackElo.append(int(game.headers["BlackElo"]))
@@ -28,6 +29,14 @@ for i in range(25000):
 		temp = temp.replace('\n','')
 		moves.append(temp)
 		movescores.append(stockfish_df['MoveScores'][i])
+	else:
+		Event_Test.append(int(game.headers["Event"]))
+		exporter = chess.pgn.StringExporter(headers=True, variations=True, comments=True)
+		pgn_string = game.accept(exporter)
+		temp = pgn_string.split('\n\n')[1]
+		temp = temp.replace('\n','')
+		moves_Test.append(temp)
+		movescores_Test.append(stockfish_df['MoveScores'][i])
 
 data_df = pd.DataFrame(index = range(0,len(Event)), columns = ['Event', 'WhiteElo', 'BlackElo', 'Moves', 'MoveScores'])
 data_df["Event"] = pd.DataFrame(Event)
@@ -36,7 +45,14 @@ data_df["BlackElo"] = pd.DataFrame(BlackElo)
 data_df["Moves"] = pd.DataFrame(moves)
 data_df["MoveScores"] = pd.DataFrame(movescores)
 
-data_df = data_df[['Event','WhiteElo','BlackElo','Moves','MoveScores']]
+datatest_df = pd.DataFrame(index = range(0,len(Event_Test)), columns = ['Event', 'Moves', 'MoveScores'])
+datatest_df["Event"] = pd.DataFrame(Event_Test)
+datatest_df["Moves"] = pd.DataFrame(moves_Test)
+datatest_df["MoveScores"] = pd.DataFrame(movescores_Test)
 
-data_df.to_csv("data.csv", index=False)
+data_df = data_df[['Event','WhiteElo','BlackElo','Moves','MoveScores']]
+datatest_df = datatest_df[['Event','Moves','MoveScores']]
+
+data_df.to_csv("kaggledata.csv", index=False)
+datatest_df.to_csv("kaggletest.csv",index=False)
 
--- worlds_dataprocessing.py
+++ worlds_dataprocessing.py
@@ -2,6 +2,7 @@ import chess
 import chess.pgn
 import chess.uci
 import pandas as pd
+import numpy as np
 
 classicalpgn = open("classical.pgn")
 rapidpgn = open("rapid.pgn")
@@ -15,23 +16,35 @@ classical_movescores = []
 temp = []
 event = []
 event_num = 1
-
 while event_num < 13:
 	classical_games = chess.pgn.read_game(classicalpgn)
+	#node = classical_games.variations[0]
+	#board = classical_games.board()
+	#classical_games = node
+	i = 1
 	while not classical_games.is_end():
 		node = classical_games.variations[0]
 		board = classical_games.board()
 		engine.position(board)
 		evaluation = engine.go(movetime=evaltime)
-		temp.append(handler.info["score"][1].cp)
+		if i % 2 == 0:
+			temp.append(handler.info["score"][1].cp*-1)
+		else:
+			temp.append(handler.info["score"][1].cp)
 		classical_games = node
+		i+=1
 	classical_movescores.append(temp)
+	print(len(temp))
 	event.append(event_num)
 	event_num += 1
 	temp = []
+	i = 1
 
 classical_df = pd.DataFrame(index = range(0,len(event)), columns = ['Event', 'MoveScores'])
 classical_df["Event"] = pd.DataFrame(event)
-classical_df["MoveScores"] = pd.DataFrame(classical_movescores)
+test = pd.Series(classical_movescores)
+classical_df["MoveScores"] = pd.DataFrame(test)
+
+print(classical_df)
 
 classical_df.to_csv("worlds_classical.csv", index=False)
